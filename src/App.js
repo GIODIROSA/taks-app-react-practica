234a393ef7c8148239efac928b28669389c1f8cd
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-05-18 23:05:03 -0400

    practica de props y componente en el traspaso de data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
+import tasks from "./sample/tasks.json";
 
+import Tasks from "./Components/Tasks";
+
+//console.log(tasks);
+
+class App extends Component {
+  state = {
+    tasks: tasks,
+  };
+
+  render() {
+    return (
+      <div>
+        <Tasks tasks={this.state.tasks} />
+      </div>
+    );
+  }
+}
 
 export default App;
