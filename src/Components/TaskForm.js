d30358f7102ee7e31b3846b835f7ec7ca1855f4d
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-05-22 21:58:34 -0400

    se termina el form e ingresa datos a la lista de tareas

--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
 
 export default class TaskForm extends Component {
+  state = {
+    title: "",
+    description: "",
+  };
+  onSubmit = (e) => {
+    this.props.addTask(this.state.title, this.state.description);
+    e.preventDefault();
+  };
+
+  onChange = (e) => {
+    // console.log(e.target.name, e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
     return (
-      <form>
-        <label for="TAREA">Escribir una tarea</label>
-        <input id="TAREA" type="text" placeholder="Escribe una tarea" />
+      <form onSubmit={this.onSubmit}>
+        <label>Escribir una tarea</label>
+        <input
+          id="tarea"
+          name="title"
+          type="text"
+          placeholder="Escribe una tarea"
+          onChange={this.onChange}
+          value={this.state.title}
+        />
         <br></br>
-        <textarea placeholder="Escribe una Descripción"></textarea>
+        <textarea
+          name="description"
+          placeholder="Escribe una Descripción"
+          onChange={this.onChange}
+          value={this.state.description}
+        ></textarea>
+        <br></br>
+        <button type="submit">Enviar</button>
       </form>
     );
   }
