b72370e2e261ba3c751d34d5eb3c22586fdb46d1
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-05-23 02:10:27 -0400

    router y manera de enrutar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 //Importando API datos
 import tasks from "./sample/tasks.json";
@@ -51,13 +52,30 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TaskForm addTask={this.addTask} />
-        <Tasks
-          tasks={this.state.tasks}
-          deleteTask={this.deleteTask}
-          checkDone={this.checkDone}
-        />
-        <Posts />
+        <Router>
+          <Link to="/">Home</Link>
+          <br></br>
+          <Link to="/posts">Post</Link>
+
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <div>
+                  <TaskForm addTask={this.addTask} />
+                  <Tasks
+                    tasks={this.state.tasks}
+                    deleteTask={this.deleteTask}
+                    checkDone={this.checkDone}
+                  />
+                </div>
+              );
+            }}
+          ></Route>
+
+          <Route path="/posts" component={Posts}></Route>
+        </Router>
       </div>
     );
   }
